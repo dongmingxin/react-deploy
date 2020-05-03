37c2215f91994a401e530cfecb822a12839c60da
Mingxin Dong <836514519dmx@gmail.com>
2020-05-04 01:57:03 +1000

    Updates

---
+++ precache-manifest.6c2bbf289c98837d70f1b0edd26b17de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6b255f674d28fa7edffb35a4325c7be",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "94bf507875c2070b3cdb",
+    "url": "/react-deploy/static/css/main.d7008831.chunk.css"
+  },
+  {
+    "revision": "a1bc7dcb0ddda7c2efa0",
+    "url": "/react-deploy/static/js/2.d6d43cce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy/static/js/2.d6d43cce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94bf507875c2070b3cdb",
+    "url": "/react-deploy/static/js/main.d84639c1.chunk.js"
+  },
+  {
+    "revision": "4d275ecffff8f8311b08",
+    "url": "/react-deploy/static/js/runtime-main.8e4be4b4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
